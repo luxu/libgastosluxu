0e1c81eeded403416bd7d9dd2bb92e70eb923b9a
Luciano Martins <zicadopv@gmail.com>
2020-10-24 10:54:46 -0300

    Adicionado mais um pasta para ignorar... .idea e o pacote libgastosluxu dentro de libgastosluxu

---
+++ libgastosluxu/github_api.py
@@ -0,0 +1,16 @@
+from requests import get
+
+
+def buscar_avatar(usuario):
+    """
+    Busca o avatar de um usuário no Github
+
+    :param usuario: str com o nome de usuário ni github
+    :return: str com o link do avatar
+    """
+    url = f'https://api.github.com/users/{usuario}'
+    resp = get(url)
+    return resp.json()['avatar_url']
+
+if __name__ == '__main__':
+    print(buscar_avatar('luxu'))
