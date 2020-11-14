1fa27ad9138c6be61b37e744249ac2c686aacb14
Luciano Martins <zicadopv@gmail.com>
2020-11-14 11:46:09 -0300

    Finalizando o capítulo 3: Injeção de Dependências sem Mock
    close #20

--- libgastosluxu/tests/test_spam/test_envio_para_base_de_usuarios.py
+++ libgastosluxu/tests/test_spam/test_envio_para_base_de_usuarios.py
@@ -1,10 +1,59 @@
+import pytest
+
 from libgastosluxu.spam.enviador_de_email import Enviador
 from libgastosluxu.spam.main import EnviadorDeSpam
+from libgastosluxu.spam.modelos import Usuario
 
 
-def test_envio_de_spam(sessao):
-    enviador_de_spam = EnviadorDeSpam(sessao, Enviador())
+@pytest.mark.parametrize(
+    'usuarios',
+    [
+        [
+            Usuario(nome='Luciano', email='contato@luxu.com.br'),
+            Usuario(nome='Renzo', email='contato@luxu.com.br')
+        ],
+        [
+            Usuario(nome='Luciano', email='contato@luxu.com.br'),
+        ]
+    ]
+)
+def test_qde_de_spam(sessao, usuarios):
+    for usuario in usuarios:
+        sessao.salvar(usuario)
+    enviador = EnviadorMock()
+    enviador_de_spam = EnviadorDeSpam(sessao, enviador)
     enviador_de_spam.enviar_emails(
+        'contato@luxu.com.br',
+        'Curso Python Pro',
+        'Confira os módulos fantásticos'
+    )
+    assert len(usuarios) == enviador.qtd_email_enviados
+
+
+class EnviadorMock(Enviador):
+
+    def __init__(self):
+        super().__init__()
+        self.qtd_email_enviados = 0
+        self.parametros_de_envio = None
+
+    def enviar(self, remetente, destinatario, assunto, corpo):
+        self.parametros_de_envio = (remetente, destinatario, assunto, corpo)
+        self.qtd_email_enviados += 1
+
+
+def test_parametros_de_spam(sessao):
+    usuario = Usuario(nome='Luciano', email='contato@luxu.com.br')
+    sessao.salvar(usuario)
+    enviador = EnviadorMock()
+    enviador_de_spam = EnviadorDeSpam(sessao, enviador)
+    enviador_de_spam.enviar_emails(
+        'publicarnowordpress@luxu.com.br',
+        'Curso Python Pro',
+        'Confira os módulos fantásticos'
+    )
+    assert enviador.parametros_de_envio == (
+        'publicarnowordpress@luxu.com.br',
         'contato@luxu.com.br',
         'Curso Python Pro',
         'Confira os módulos fantásticos'
--- libgastosluxu/tests/test_spam/tests_usuarios.py
+++ libgastosluxu/tests/test_spam/tests_usuarios.py
@@ -2,15 +2,15 @@ from libgastosluxu.spam.modelos import Usuario
 
 
 def test_salvar_usuario(sessao):
-    usuario = Usuario(nome='Luciano')
+    usuario = Usuario(nome='Luciano', email='contato@luxu.com.br')
     sessao.salvar(usuario)
     assert isinstance(usuario.id, int)
 
 
 def test_listar_usuarios(sessao):
     usuarios = [
-        Usuario(nome='Luciano'),
-        Usuario(nome='Renzo')
+        Usuario(nome='Luciano', email='contato@luxu.com.br'),
+        Usuario(nome='Renzo', email='contato@luxu.com.br')
     ]
     for usuario in usuarios:
         sessao.salvar(usuario)
