8d25d5e67cd761655eb426527d5b6a39ee3f6003
Luciano Martins <zicadopv@gmail.com>
2020-11-26 07:16:16 -0300

    Finalizando o capítulo 4: Módulo como Objeto - Criei um teste para CEP
    close #26

--- libgastosluxu/github_api.py
+++ libgastosluxu/github_api.py
@@ -1,13 +1,23 @@
 import requests
 
-
 def buscar_avatar(usuario):
     """
     Busca o avatar de um usuário no Github
 
-    :param usuario: str com o nome de usuário ni github
+    :param usuario: str com o nome de usuário no github
     :return: str com o link do avatar
     """
     url = f'https://api.github.com/users/{usuario}'
     resp = requests.get(url)
     return resp.json()['avatar_url']
+
+def busca_cep(nro_cep):
+    """
+        Busca as informações de um determinado CEP
+
+        :param usuario: int numero do cep
+        :return: str informações do logradouro
+    """
+    url = f'http://www.viacep.com.br/ws/{nro_cep}/json'
+    resp = requests.get(url)
+    return resp.json()['logradouro']
